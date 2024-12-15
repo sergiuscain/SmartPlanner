4db1119523c971494c0e7cb4a2dbd0c40815d3a2
sergiuscain <sergiussmithe@gmail.com>
2024-12-15 22:35:28 +0300

    Добавил тестовую заметку

--- SmartPlanner/TestData/DataForTesting.cs
+++ SmartPlanner/TestData/DataForTesting.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SmartPlanner.Data.Entities;
 using SmartPlanner.Models;
 
 namespace SmartPlanner.TestData
@@ -7,6 +8,13 @@ namespace SmartPlanner.TestData
     {
         public static Guid UserId = Guid.Parse("22c98a74-2913-4284-8b9c-b3e528838ba1");
         public static List<NoteViewModel> Notes { get; } = new List<NoteViewModel>();
+        public static Note TestNote { get; } = new Note 
+        {
+            Id = new Guid(),
+            Title = $"Title for test",
+            Description = $"Description for test",
+            UserId = UserId
+        };
         static DataForTesting()
         {
             for (var i = 0; i < 10; i++)
