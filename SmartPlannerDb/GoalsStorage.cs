a5f5f64542b0a3afeb2e2acf9d016651202a6062
sergiuscain <sergiussmithe@gmail.com>
2024-12-24 12:46:35 +0300

    Добавил хранилище целей и его интерфейс

--- SmartPlannerDb/GoalsStorage.cs
+++ SmartPlannerDb/GoalsStorage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SmartPlannerDb.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace SmartPlannerDb
 {
-    public class GoalsStorage
+    public class GoalsStorage : IGoalsStorage
     {
+        private readonly DataContext _context;
+        public GoalsStorage(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Goal goal)
+        {
+            _context.Goals.Add(goal);
+            await _context.SaveChangesAsync();
+        }
+        public async Task Delete(Guid id)
+        {
+            var goal = await _context.Goals.FirstOrDefaultAsync(x => x.Id == id);
+            if (goal != null)
+            {
+                _context.Goals.Remove(goal);
+                await _context.SaveChangesAsync();
+            }
+        }
+        public async Task<List<Goal>> GetAllAsync(string userId)
+        {
+            var goals = _context.Goals.Where(x => x.UserId == userId);
+            return await goals.ToListAsync();
+        }
+        public async Task<Goal> GetById(Guid id)
+        {
+            return await _context.Goals.FirstOrDefaultAsync(g => g.Id == id);
+        }
+        public async Task AddPoints(Guid goalId, int point)
+        {
+            var goal = await GetById(goalId);
+            goal.CurrentProgress += point;
+            await _context.SaveChangesAsync();
+        }
     }
 }
---
+++ SmartPlannerDb/IGoalsStorage.cs
@@ -0,0 +1,13 @@
+﻿using SmartPlannerDb.Entities;
+
+namespace SmartPlannerDb
+{
+    public interface IGoalsStorage
+    {
+        Task AddAsync(Goal goal);
+        Task AddPoints(Guid goalId, int point);
+        Task Delete(Guid id);
+        Task<List<Goal>> GetAllAsync(string userId);
+        Task<Goal> GetById(Guid id);
+    }
+}
